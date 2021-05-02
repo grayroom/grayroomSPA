e93a3cbdee449c3e84467b0786acc24da98dca86
grayroom <leejh9226@gmail.com>
2021-05-02 05:15:54 +0000

    gh-pages

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import 'foundation-sites/dist/css/foundation.min.css';
+import './default-set.css'
 import style from './style.module.css';
 
 import { Button, Colors } from 'react-foundation';
 
+
+const appRoot = document.getElementById("app-root")
+const modalRoot = document.getElementById("modal-root")
+
 function ShadowBorder(props) {
 	return (
-		<div class={style.dialog}>
+		<div className={style.dialog}>
 			{props.children}
 		</div>
 	)
@@ -17,10 +22,10 @@ function ShadowBorder(props) {
 function Dialog(props) {
 	return (
 		<ShadowBorder color="black">
-			<h1 class="dialog-title">
+			<h1 className="dialog-title">
 				{props.title}
 			</h1>
-			<p class="dialog-message">
+			<p className="dialog-message">
 				{props.message}
 			</p>
 			{props.children}
@@ -28,13 +33,38 @@ function Dialog(props) {
 	)
 }
 
+class Modal extends React.Component {
+	constructor(props) {
+		super(props)
+		this.el = document.createElement("div")
+	}
+	
+	componentDidMount() {
+		modalRoot.appendChild(this.el)
+	}
+	
+	componentWillUnmount() {
+		modalRoot.removeChild(this.el)
+	}
+	
+	render() {
+		return ReactDOM.createPortal(
+			this.props.children,
+			this.el
+		)
+	}
+}
+
 class SignupDialog extends React.Component {
 	constructor(props) {
 		super(props)
 		this.handleID = this.handleID.bind(this)
 		this.handlePW = this.handlePW.bind(this)
+		this.handleShowAddress = this.handleShowAddress.bind(this)
+		this.handleHideAddress = this.handleHideAddress.bind(this)
 		this.handleSignUp = this.handleSignUp.bind(this)
-		this.state = {ID: "", PW: ""}
+		
+		this.state = {ID: "", PW: "", showAddress: false}
 	}
 	
 	handleID(e) {
@@ -45,22 +75,40 @@ class SignupDialog extends React.Component {
 		this.setState({PW: e.target.value})
 	}
 	
+	handleShowAddress(e) {
+		this.setState({showAddress: true})
+	}
+	
+	handleHideAddress(e) {
+		this.setState({showAddress: false})
+	}
+	
 	handleSignUp() {
 		alert(`Welcome aborad, ${this.state.ID}`)
 	}
 	
 	render() {
+		const modal = this.state.showAddress ? (
+					<Modal>
+						<div className="modal" onClick={this.handleHideAddress}>
+							<h1>주소검색</h1>
+							<p>asdf</p>
+						</div>
+					</Modal> 
+					) : null
+		
 		return (
 			<Dialog title="로그인"
 					message="반갑습니다">
 				<input type="text" value={this.state.ID} onChange={this.handleID} placeholder="ID" />
 				<input type="text" value={this.state.PW} onChange={this.handlePW} placeholder="PW" />
-				<Button color={Colors.SUCCESS} onClick={this.handleSignUp}>SignUp</Button>
+				<Button color={Colors.SECONDARY} onClick={this.handleShowAddress}>주소</Button>
+				{modal}
+				<Button color={Colors.SUCCESS} onClick={this.handleSignUp}>제출</Button>
 			</Dialog>
 		)
 	}
 }
 
-ReactDOM.render(
-	<SignupDialog />, document.getElementById("root")
-)
+
+ReactDOM.render(<SignupDialog />, appRoot)
